e55e29f7aa3929d7b20d977832ae8d4dd2013b3c
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-05-08 01:13:15 +0300

    slide-show fix

--- slide-show-animation.js
+++ slide-show-animation.js
@@ -8,16 +8,22 @@ document.addEventListener('DOMContentLoaded', function (event) {
     let backgrounds = document.getElementById('backgrounds').children;
 
     let currentBackground = backgrounds[currentIndex];
-    let nextBackground;
-    setInterval(function(){ 
-        currentIndex++;
+    let nextBackground = backgrounds[currentIndex + 1];
 
+    let selected = false;
+    setInterval(function(){
+        if(!selected){
+        currentBtn = navBtns[currentIndex];
+        currentBtn.classList.remove('active');
+
+        currentBackground = backgrounds[currentIndex];
+        currentBackground.classList.add('backward');
+        currentBackground.classList.remove("current");
+
+        currentIndex++;
         if(currentIndex > backgrounds.length - 1){
             currentIndex = 0;
         }
-
-        currentBtn.classList.remove("active");
-        currentBackground.classList.add('backward');
 
         currentBtn = navBtns[currentIndex];
         currentBtn.classList.add("active");
@@ -28,13 +34,35 @@ document.addEventListener('DOMContentLoaded', function (event) {
         setTimeout(() => {
             currentBackground.classList.remove('backward');
             currentBackground = nextBackground;
+
         }, 2000);
+        }
+        selected = false;
     }, 5000);
 
     nav.addEventListener("click", function(event){
         if(event.target.tagName === "LI"){
-            console.log(event.target.indexOf);
-        }
+            let index = Array.from(navBtns).indexOf(event.target);
+            if(currentIndex != index){
+                currentIndex = index;
+                selected = true;
 
+                currentBackground.classList.remove("current");
+                currentBackground.classList.remove("backward");
+
+                nextBackground.classList.remove("current");
+                nextBackground.classList.remove("backwards");
+                
+                currentBtn.classList.remove("active");
+
+                currentBtn = navBtns[index];
+                currentBtn.classList.add("active");
+                 
+
+                currentBackground = backgrounds[index];
+                currentBackground.classList.remove('backward');
+                currentBackground.classList.add('current');
+            }
+        }
     })
 });
