a5fb92ce286e7530d5c5fbc01906e3864f5fda24
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-05-23 19:54:39 +0300

    header logic moved to header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service'
 
 
 @Component({
@@ -10,7 +9,7 @@ import { AuthService } from './services/auth.service'
 export class AppComponent {
   title = 'Extension-repository';
 
-  constructor(private authService : AuthService) { 
+  constructor() { 
   }
 
 }
