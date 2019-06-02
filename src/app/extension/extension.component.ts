26c5481b6056157766f110436c1635f517417e70
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-02 14:53:41 +0300

    extension component - menu

--- src/app/extension/extension.component.ts
+++ src/app/extension/extension.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ExtensionsService } from '../services/extensions.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-extension',
@@ -11,7 +12,7 @@ export class ExtensionComponent implements OnInit {
 
   extension : any
 
-  constructor(private extensionService : ExtensionsService, private route: ActivatedRoute) { }
+  constructor(private extensionService : ExtensionsService, private authService : AuthService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.extension = []
