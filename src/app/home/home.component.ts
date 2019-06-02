c31611678978824728e67902699940b1b8b7bf2b
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-02 15:14:49 +0300

    small fix in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,14 +21,11 @@ import { ProfileComponent } from '../profile/profile.component';
 })
 export class HomeComponent implements OnInit {
 
-  isLoggedIn : boolean;
   extensions : any;
 
   constructor(private authService : AuthService, private extensionsService : ExtensionsService ) { }
 
   ngOnInit() {
-    this.isLoggedIn = this.authService.isLoggedIn
-
     this.extensionsService.getFeatured().subscribe(data => {
       this.extensions = data;
     })
