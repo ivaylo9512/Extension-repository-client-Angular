969f3f86ab14616378085a285a9eff31ea4ba14c
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-05 02:20:47 +0300

    small fix - home view mousewheel animation

--- src/app/helpers/mouse-wheel.directive.ts
+++ src/app/helpers/mouse-wheel.directive.ts
@@ -7,7 +7,9 @@ export class MouseWheelDirective {
   profileComonent = {
     profileSection : undefined,
     extensionsSection : undefined,
-    currentSection : 'profileSection'
+    circle : undefined,
+    currentSection : 'profileSection',
+    display : true
   }
 
   @ViewChild('tagsContainer') tagsContainer : ElementRef
@@ -19,24 +21,37 @@ export class MouseWheelDirective {
     if(!this.profileComonent.profileSection){
       this.profileComonent.profileSection = document.getElementById('profile-section')
       this.profileComonent.extensionsSection = document.getElementById('extensions-section')
+      this.profileComonent.circle = document.getElementById('circle')
     }
 
     const profileSection = this.profileComonent.profileSection
     const extensionsSection = this.profileComonent.extensionsSection
-    
+    const circle = this.profileComonent.circle
+
     if(this.profileComonent.currentSection == 'profileSection'){
       if (e.deltaY > 0) {
         profileSection.classList.remove('fade-out')
-        profileSection.classList.add('translate-profile-section')
-        extensionsSection.style.display = 'block'    
-        this.profileComonent.currentSection = 'extensionsSection'    
+        profileSection.classList.add('fade-in')
+        circle.classList.add('animate')
+        this.profileComonent.currentSection = 'extensionsSection'
+        this.profileComonent.display = true
+
+        setTimeout(() => {
+          const animationFinished = window.getComputedStyle(profileSection).getPropertyValue("opacity") == '1'
+          console.log(animationFinished)
+          if(this.profileComonent.display && animationFinished){
+            extensionsSection.style.display = 'block'    
+          }
+        }, 4100);
       }
     }else{
       if (e.deltaY < 0 && window.scrollY == 0) {
-        profileSection.classList.remove('translate-profile-section')
+        profileSection.classList.remove('fade-in')
+        circle.classList.remove('animate')
         profileSection.classList.add('fade-out')
         extensionsSection.style.display = 'none'
         this.profileComonent.currentSection = 'profileSection'
+        this.profileComonent.display = false
       }
     }
   
