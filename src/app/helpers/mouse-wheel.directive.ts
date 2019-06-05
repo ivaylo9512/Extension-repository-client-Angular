5582095d3608433e891dd1e3164ea03312f7322a
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-05 22:58:15 +0300

    create component - scroll animation

--- src/app/helpers/mouse-wheel.directive.ts
+++ src/app/helpers/mouse-wheel.directive.ts
@@ -1,65 +1,138 @@
-import { Directive, Output, HostListener, EventEmitter,  ElementRef, ViewChild } from '@angular/core';
+import { Directive,OnInit, Output, HostListener, EventEmitter,  ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Directive({ selector: '[mouseWheel]' })
-export class MouseWheelDirective {
-  @Output() mouseWheelUp = new EventEmitter();
-  @Output() mouseWheelDown = new EventEmitter();
-  profileComonent = {
+export class MouseWheelDirective implements OnInit {
+  profileComponent = {
     profileSection : undefined,
     extensionsSection : undefined,
     circle : undefined,
     currentSection : 'profileSection',
     display : true
   }
+  submitComponent = {
+    coverSection : undefined,
+    extensionSection : undefined,
+    previewSection : undefined,
+    circle : undefined,
+    currentSection : 'coverSection',
+    animated : true
+  }
+  currentComponent : string
 
   @ViewChild('tagsContainer') tagsContainer : ElementRef
-  constructor(){
+  constructor(private authService : AuthService, private route: ActivatedRoute){
   }
 
   @HostListener('wheel', ['$event']) 
   Wheel(e) {
-    if(!this.profileComonent.profileSection){
-      this.profileComonent.profileSection = document.getElementById('profile-section')
-      this.profileComonent.extensionsSection = document.getElementById('extensions-section')
-      this.profileComonent.circle = document.getElementById('circle')
+    switch(this.currentComponent){
+      case 'HomeComponent' :
+        this.homeAnimation(e)
+        break;
+      case 'CreateComponent' :
+      case 'EditComponent' :
+        this.submitAnimation(e)
+        break;
     }
+  }
 
-    const profileSection = this.profileComonent.profileSection
-    const extensionsSection = this.profileComonent.extensionsSection
-    const circle = this.profileComonent.circle
+  homeAnimation(e){
+    if(this.authService.isLoggedIn){
 
-    if(this.profileComonent.currentSection == 'profileSection'){
+      const profileSection = this.profileComponent.profileSection
+      const extensionsSection = this.profileComponent.extensionsSection
+      const circle = this.profileComponent.circle
+
+      if(this.profileComponent.currentSection == 'profileSection'){
+        if (e.deltaY > 0) {
+          profileSection.classList.remove('fade-out')
+          profileSection.classList.add('fade-in')
+          circle.classList.add('animate')
+          this.profileComponent.currentSection = 'extensionsSection'
+          this.profileComponent.display = true
+
+          setTimeout(() => {
+            const animationFinished = window.getComputedStyle(profileSection).getPropertyValue("opacity") == '1'
+            if(this.profileComponent.display && animationFinished){
+              extensionsSection.style.display = 'block'    
+            }
+          }, 4100);
+        }
+      }else{
+        if (e.deltaY < 0 && window.scrollY == 0) {
+          profileSection.classList.remove('fade-in')
+          circle.classList.remove('animate')
+          profileSection.classList.add('fade-out')
+          extensionsSection.style.display = 'none'
+          this.profileComponent.currentSection = 'profileSection'
+          this.profileComponent.display = false
+        }
+      }
+    }
+  }
+  submitAnimation(e){
+    const coverSection = this.submitComponent.coverSection
+    const extensionsSection = this.submitComponent.extensionSection
+    const previewSection = this.submitComponent.previewSection
+    const circle = this.submitComponent.circle
+    e.preventDefault()
+
+    if(this.submitComponent.currentSection == 'coverSection'){
       if (e.deltaY > 0) {
-        profileSection.classList.remove('fade-out')
-        profileSection.classList.add('fade-in')
+        extensionsSection.classList.add('fade-in')
         circle.classList.add('animate')
-        this.profileComonent.currentSection = 'extensionsSection'
-        this.profileComonent.display = true
+        this.submitComponent.currentSection = 'extensionSection'
+        this.submitComponent.animated = false
 
         setTimeout(() => {
-          const animationFinished = window.getComputedStyle(profileSection).getPropertyValue("opacity") == '1'
-          console.log(animationFinished)
-          if(this.profileComonent.display && animationFinished){
-            extensionsSection.style.display = 'block'    
-          }
-        }, 4100);
+          this.submitComponent.animated = true
+        }, 3000);
+      }
+    }else if(this.submitComponent.currentSection == 'extensionSection'){
+      if(e.deltaY < 0 && this.submitComponent.animated){
+        extensionsSection.classList.remove('fade-in')
+        circle.classList.remove('animate')
+        this.submitComponent.currentSection = 'coverSection'
+      }else if(this.submitComponent.animated){
+        previewSection.classList.add('fade-in')
+        this.submitComponent.currentSection = 'previewSection'
+        this.submitComponent.animated = false
+        setTimeout(() => {
+          this.submitComponent.animated = true
+        }, 3000);
       }
     }else{
-      if (e.deltaY < 0 && window.scrollY == 0) {
-        profileSection.classList.remove('fade-in')
-        circle.classList.remove('animate')
-        profileSection.classList.add('fade-out')
-        extensionsSection.style.display = 'none'
-        this.profileComonent.currentSection = 'profileSection'
-        this.profileComonent.display = false
+      if(e.deltaY < 0 && this.submitComponent.animated){
+        previewSection.classList.remove('fade-in')
+        this.submitComponent.currentSection = 'extensionSection'
+
+        this.submitComponent.animated = false
+        setTimeout(() => {
+          this.submitComponent.animated = true
+        }, 3000);
       }
     }
-  
   }
-
-  mouseWheelFunc(event: any) {
+  ngOnInit() {
+    this.currentComponent = this.route.component['name']
   }
-  mouseWheelUpFunc(event){
+  ngAfterViewChecked(){
+    switch(this.currentComponent){
+      case 'HomeComponent' :
+        this.profileComponent.profileSection = document.getElementById('profile-section')
+        this.profileComponent.extensionsSection = document.getElementById('extensions-section')
+        this.profileComponent.circle = document.getElementById('circle')
+        break;
+      case 'CreateComponent' :
+      case 'EditComponent' :
+        this.submitComponent.coverSection = document.getElementById('cover-section')
+        this.submitComponent.extensionSection = document.getElementById('extension-section')
+        this.submitComponent.previewSection = document.getElementById('preview-section')
+        this.submitComponent.circle = document.getElementById('circle')
+        break;    
+    }
   }
 
 }
