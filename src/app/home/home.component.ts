f09fb8503e6e63ab4d101fd4376c705bb0382790
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-08 06:49:43 +0300

    home componend - slide show

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AuthService } from '../services/auth.service'
 import { ExtensionsService } from '../services/extensions.service';
 
@@ -22,13 +22,46 @@ import { ProfileComponent } from '../profile/profile.component';
 export class HomeComponent implements OnInit {
 
   extensions : any;
+  @ViewChild('backgroundsContainer') backgroundsContainer : ElementRef
 
-  constructor(private authService : AuthService, private extensionsService : ExtensionsService ) { }
+  constructor(private authService : AuthService, private extensionsService : ExtensionsService ) {
+   }
 
   ngOnInit() {
     this.extensionsService.getFeatured().subscribe(data => {
       this.extensions = data;
+      this.setSlideShow()
     })
   }
 
+  setSlideShow(){
+    let currentIndex = 0
+    const backgrounds = this.backgroundsContainer.nativeElement.children
+
+    let currentBackground = backgrounds[currentIndex];
+    let nextBackground = backgrounds[currentIndex + 1];
+
+    let selected = false;
+    setInterval(() => {
+        if(!selected){
+            currentBackground = backgrounds[currentIndex]
+            currentBackground.classList.add('backward')
+            currentBackground.classList.remove("current")
+
+            currentIndex++
+            if(currentIndex > backgrounds.length - 1){
+                currentIndex = 0
+            }
+
+            nextBackground = backgrounds[currentIndex]
+            nextBackground.classList.add('current')
+
+            setTimeout(() => {
+                currentBackground.classList.remove('backward');
+                currentBackground = nextBackground
+            }, 2000);
+        }
+        selected = false;
+    }, 5000);
+  }
 }
