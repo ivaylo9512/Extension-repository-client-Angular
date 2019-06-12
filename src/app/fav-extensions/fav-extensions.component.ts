1e86957a522027ea909871859dc5e1bf54102033
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-12 12:09:11 +0300

    moving logic to favourite extensions component

--- src/app/fav-extensions/fav-extensions.component.ts
+++ src/app/fav-extensions/fav-extensions.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, QueryList, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { ExtensionsService } from '../services/extensions.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-fav-extensions',
@@ -7,9 +9,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavExtensionsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('backgroundsContainer') backgroundsContainer : ElementRef
+  @ViewChildren('extensionDescriptions') extensionDescriptions : QueryList<any>
 
-  ngOnInit() {
+  extensions : any[]
+  currentIndex : number
+  initial : boolean
+
+  constructor(private authService : AuthService, private extensionsService : ExtensionsService, private cdRef : ChangeDetectorRef) { 
+    this.currentIndex = 0
+    this.initial = true
+    this.extensions = undefined
   }
 
+  ngOnInit() {
+    this.extensionsService.getFeatured().subscribe(data => {
+      this.extensions = data;
+      this.setSlideShow()
+    })
+  }
+
+  setSlideShow(){
+    const backgrounds = this.backgroundsContainer.nativeElement.children
+
+    let currentBackground = backgrounds[this.currentIndex];
+    let nextBackground = backgrounds[this.currentIndex + 1];
+
+    let selected = false;
+    setInterval(() => {
+        if(!selected){
+            currentBackground = backgrounds[this.currentIndex]
+            currentBackground.classList.add('backward')
+            currentBackground.classList.remove("current")
+
+            this.currentIndex++
+            if(this.currentIndex > backgrounds.length - 1){
+              this.currentIndex = 0
+            }
+
+            nextBackground = backgrounds[this.currentIndex]
+            nextBackground.classList.add('current')
+
+            setTimeout(() => {
+                currentBackground.classList.remove('backward');
+                currentBackground = nextBackground
+            }, 2000);
+        }
+        selected = false;
+    }, 5000);
+  }
+
+  ngAfterViewInit() {
+    this.extensionDescriptions.changes.subscribe(descriptions => {
+      descriptions.toArray().forEach(description => {
+      
+        let height = description.nativeElement.offsetHeight
+        let scrollHeight = description.nativeElement.scrollHeight
+        let text = description.nativeElement.innerHTML + '...'
+      
+        while(height < scrollHeight){
+          let words = text.split(' ')
+          words.pop()
+          words.pop()
+          text = words.join(' ') + '...'
+          
+          description.nativeElement.innerHTML = text
+          height = description.nativeElement.offsetHeight
+          scrollHeight = description.nativeElement.scrollHeight
+        }
+      })
+      this.initial = false
+      this.cdRef.detectChanges();
+    })
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
 import { MouseWheelDirective } from '../helpers/mouse-wheel.directive';
+import { FavExtensionsComponent } from '../fav-extensions/fav-extensions.component';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +16,7 @@ import { MouseWheelDirective } from '../helpers/mouse-wheel.directive';
 @NgModule({
   declarations: [
     ProfileComponent,
+    FavExtensionsComponent
   ],
   imports: [
     BrowserModule,
@@ -22,76 +24,11 @@ import { MouseWheelDirective } from '../helpers/mouse-wheel.directive';
 })
 export class HomeComponent implements OnInit {
 
-  extensions : any[]
-  currentIndex : number
-  initial : boolean
 
-  @ViewChild('backgroundsContainer') backgroundsContainer : ElementRef
-  @ViewChildren('extensionDescriptions') extensionDescriptions : QueryList<any>
-
-  constructor(private authService : AuthService, private extensionsService : ExtensionsService, private cdRef : ChangeDetectorRef ) {
-    this.currentIndex = 0
-    this.initial = true
-    this.extensions = undefined
+  constructor(private authService : AuthService) {
   }
 
   ngOnInit() {
-    this.extensionsService.getFeatured().subscribe(data => {
-      this.extensions = data;
-      this.setSlideShow()
-    })
   }
 
-  setSlideShow(){
-    const backgrounds = this.backgroundsContainer.nativeElement.children
-
-    let currentBackground = backgrounds[this.currentIndex];
-    let nextBackground = backgrounds[this.currentIndex + 1];
-
-    let selected = false;
-    setInterval(() => {
-        if(!selected){
-            currentBackground = backgrounds[this.currentIndex]
-            currentBackground.classList.add('backward')
-            currentBackground.classList.remove("current")
-
-            this.currentIndex++
-            if(this.currentIndex > backgrounds.length - 1){
-              this.currentIndex = 0
-            }
-
-            nextBackground = backgrounds[this.currentIndex]
-            nextBackground.classList.add('current')
-
-            setTimeout(() => {
-                currentBackground.classList.remove('backward');
-                currentBackground = nextBackground
-            }, 2000);
-        }
-        selected = false;
-    }, 5000);
-  }
-  ngAfterViewInit() {
-    this.extensionDescriptions.changes.subscribe(descriptions => {
-      descriptions.toArray().forEach(description => {
-      
-        let height = description.nativeElement.offsetHeight
-        let scrollHeight = description.nativeElement.scrollHeight
-        let text = description.nativeElement.innerHTML + '...'
-      
-        while(height < scrollHeight){
-          let words = text.split(' ')
-          words.pop()
-          words.pop()
-          text = words.join(' ') + '...'
-          
-          description.nativeElement.innerHTML = text
-          height = description.nativeElement.offsetHeight
-          scrollHeight = description.nativeElement.scrollHeight
-        }
-      })
-      this.initial = false
-      this.cdRef.detectChanges();
-    })
-  }
 }
